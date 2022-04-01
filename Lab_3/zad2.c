47af8a82be9b43e6c7f6939854e03a6b74111746
Uiskam <92760953+Uiskam@users.noreply.github.com>
2022-04-01 18:08:07 +0200

    funcitons implemented, now lets build a program

--- Lab_3/zad2.c
+++ Lab_3/zad2.c
@@ -3,7 +3,20 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#define NULL_PID -1
+void wyp(int tab[], int size) {
+    for(int i = 0; i < size; i++)
+        printf("%d ", tab[i]);
+    printf("\n");
+}
 
+void clean_prev_files(int n) {
+    char command[10];
+    for(int i = 0; i < n; i++) {
+        snprintf(command,10,"rm w%d.txt",i);    
+        system(command);
+    }
+}
 /*
  * INPUT: 2 parametry:
  *      dok - dokładnośc obliczeń (liczba prostokątów na jakie dziele przedział)
@@ -15,13 +28,46 @@
  * Wynik obliczeń powinien zostac przedstawony na standardowym wyjściu.
  * Program ma również tworzyć raport dot. czasu wykonania obliczęń w zależności od dokładności i liczby procesów.
  * (Dokładność obliczeń ma byc taka, żeby obliczneia trwały min kilka sekund)
+ * 
+ * Pradiwłowy wynik całki: pi = 3.14159265
  */
 
 int find_relative_pid(int* tab, int size) {
     int rpid = 0;
-    while (rpid < size && tab[rpid] != 0)
+    while (rpid < size && tab[rpid] != NULL_PID)
         rpid++;
-    return rpid;
+    return rpid - 1;
+}
+double f(int x) {
+    return (double)(4 / (x * x + 1));
+}
+void calculations(int pid, int square_qunatity, int proccess_count) {
+    double begin = pid * square_qunatity, square_width = proccess_count/square_qunatity;
+    double result = 0;
+    for(int i = 0; i < square_qunatity; i++) {
+        result += f(begin);
+        begin += square_width;
+    }
+    FILE* output;
+    
+    char file_name[50];
+    snprintf(file_name,50,"w%d.txt",pid);    
+    output = fopen(file_name, "w");
+    fwrite(&result, sizeof(double), 1, output);
+    fclose(output);
+}
+
+void result(int pid_count) {
+    double result = 0;
+    char file_name[50], partial_result[30];
+    FILE* result_file;
+    for(int cur_pid = 0 ; cur_pid < pid_count; cur_pid++) {
+        snprintf(file_name,50,"w%d.txt",cur_pid);
+        result_file = fopen(file_name, "r");
+        fscanf(result_file, "%s", partial_result);
+        result += atof(partial_result);
+    }
+    printf("integral calculation result %f\n", result);
 }
 
 int main(int argc, char **argv) {
@@ -29,15 +75,20 @@ int main(int argc, char **argv) {
         printf("Precisely two int arg are required!\n");
         return -1;
     }
-    int n = atoi(argv[2]), precision = atoi(argv[1]);
-    int *process_register = (int*)calloc(n,sizeof(int));
+    int  n = atoi(argv[2]);
+    double precision = atof(argv[1]);
+    printf("%f %d\n",precision, n);
+    int *process_register = (int*)malloc(n * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        process_register[i] = NULL_PID;
+    }
     if (n <= 0 || precision <= 0) {
         printf("Postive int is required as arg!\n");
         return -1;
     }
     int i_am_mother = 0;
     for (int i = 0; i < n; i++) {
-        process_register[i] = i+1;
+        process_register[i] = i;
         i_am_mother = fork();
         if(i_am_mother == 0) {
             printf("My PID is %d\n", i + 1);
