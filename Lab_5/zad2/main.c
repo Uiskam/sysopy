8ed9e30afe3d09291caddaad67e9a447a537e547
uiskam <maksulima@gmail.com>
2022-04-24 17:01:25 +0200

    tmp

--- Lab_5/zad2/main.c
+++ Lab_5/zad2/main.c
@@ -15,18 +15,34 @@
     Jeżeli program zostanie wywołany z trzema argumentami, to (za pomocą popen()) uruchamiany jest program mail i za jego pomocą wysyłany jest e-mail do określonego nadawcy z określonym tematem i treścią
 
 */
+int send_email(char* email, char* topic, char* text) {
+    char *command = malloc(sizeof text  + sizeof topic + sizeof email + sizeof(char) * 32); 
+    sprintf(command, "echo -e \"%s\" | mail -s %s %s", text, topic, email);
+    FILE* sending_pipe = popen(command, "r");
+    if(sending_pipe == NULL) {
+        perror("error while creating pipe\n");
+        return -1;
+    }
+    
+    char* return_value = malloc(sizeof(char) * 100);
+    fgets(return_value, 10, sending_pipe);
+    printf("commmunicat %s\n", return_value);
+    pclose(sending_pipe);
+    free(return_value);
+    free(command);
+    return 0;
+}
 int main(int argc, char **argv) {
     switch (argc)
     {
     case 2:
         printf("2\n");
         break;
-    case 3:
-        char *command; 
-        
+    case 4:
+        send_email(argv[1], argv[2], argv[3]);
         break;
     default:
-        printf("wrong arg number (2 or 3 are requiered)\n");
+        printf("wrong arg number (1 or 3 are requiered)\n");
         return -1;
     }
     return 0;
