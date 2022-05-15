d99a5db470be0d6b6024c3fcb5a6b1b2761ef094
uiskam <maksulima@gmail.com>
2022-05-15 21:43:52 +0200

    automatic commit

---
+++ Lab_7/zad2/comm_def.h
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <sys/ipc.h>
+
+#ifndef SYSOPY_COMM_DEF_H
+#define SYSOPY_COMM_DEF_H
+
+#define STOP 1
+#define LIST 2
+#define TWOALL 3
+#define TWOONE 4
+
+#define INIT 20
+#define SERVER_SHUT_DOWN 30
+
+#define SERVER_CAPACITY 10
+#define MAXMSG 300
+#define SERVER_QUE_KEY ftok(getpwuid(getuid())->pw_dir,0)
+#define SERVER_WAIT_TIME 5
+
+struct comm_msg {
+    long mtype;         /* typ komunikatu   */
+    long senderID;      /* id nadawcy */
+    char mtext[MAXMSG];      /* treść komunikatu */
+    int active_users[SERVER_CAPACITY];
+};
+
+
+
+#endif //SYSOPY_COMM_DEF_H
---
+++ Lab_7/zad2/deliver.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include "comm_def.h"
