5cd5df9bc52c57b18524b55c6e5a79eb326d0f33
uiskam <maksulima@gmail.com>
2022-05-24 15:58:19 +0200

    automatic commit

---
+++ Lab_9/main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+
+#define SECOND 1000000
+#define ELVES_NUMBER 10
+#define MIN_ELVES_TO_ACTION 3
+#define REINDEER_NUMBER 9
+
+void go_to_sleep(int min_sleep, int range) {
+    usleep(rand() % (SECOND * range) + SECOND * min_sleep);
+}
+
+int reindeers_counter = 0;
+int elves_queue_size = 0;
+sem_t elves_sem;
+
+void* elf(void * ID) {
+    int id = *((int *)ID);
+    printf("elfe with id: %d comes to life\n",id);
+    while (1) {
+        go_to_sleep(2,3);
+        printf("Elf %d: czeka %d elfów na Mikołaja\n",id, ++elves_queue_size);
+        if(sem_wait(&elves_sem) != 0) {
+            printf("elf %d sem_wait error", id);
+            perror("");
+        }
+        if()
+    }
+    
+}
+int main() {
+    if(sem_init(&elves_sem, 0, MIN_ELVES_TO_ACTION) == 0) {
+        perror("elves sem creation error");
+        return 0;
+    }
+
+}
